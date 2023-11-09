90b3ae3848628f2bf75d4623fa9a7c0c5a08a4c6
Giacomo Trenna <ae2342@student.jamk.fi>
2023-11-09 11:52:00 +0200

    Spawner works FLAWLESSLY

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,56 +2,124 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class SpawnArea
+{
+    public string pathName;
+    public GameObject areaGameObject;
+    public float spawnPercentage;
+}
+
 public class Spawner : MonoBehaviour
 {
-    public List<Transform> spawnPoints;
-    public List<float> spawnPercentages; // Percentage for each spawn point
-    public int totalEnemiesInWave = 50;
-    public float spawnDelay = 1.0f; // Delay in seconds between enemy spawns
+    public List<SpawnArea> spawnAreas; // Define the areas using GameObjects with Collider
+    public float minDelay = 1.0f; // Minimum delay in seconds between enemy spawns
+    public float maxDelay = 2.0f; // Maximum delay in seconds between enemy spawns
 
-    private List<Transform> availableSpawnPoints;
-    private GameObject enemyPrefab;
-    private List<int> numberEnemiesPerPoint;
+    private int _currentWave = 1;
+    private int _totalEnemiesInWave;
+    private GameObject _enemyPrefab;
+    private List<SpawnArea> _availableSpawnAreas;
+    private List<int> _numberEnemiesPerArea;
 
     void Start()
     {
-        numberEnemiesPerPoint = CalculateEnemiesPerSpawnPoint();
-        availableSpawnPoints = new List<Transform>(spawnPoints);
-        enemyPrefab = Resources.Load<GameObject>("Enemy");
-        StartCoroutine(SpawnWaveWithDelay());
+        _enemyPrefab = Resources.Load<GameObject>("Enemy");
+        CalculateAmountOfEnemies();
+        _availableSpawnAreas = new List<SpawnArea>(spawnAreas);
+        _numberEnemiesPerArea = CalculateEnemiesPerSpawnArea();
+        StartCoroutine(SpawnWaveWithRandomDelay());
     }
 
-    // Calculates and returns a list of int that represents the number of enemies to spawn per spawn points according to percentages given
-    private List<int> CalculateEnemiesPerSpawnPoint()
+    private void CalculateAmountOfEnemies()
     {
-        var enemiesToSpawnPerPoint = new List<int>();
+        _totalEnemiesInWave = _currentWave * 10;
+    }
 
-        for (var i = 0; i < spawnPoints.Count; i++)
+    void Update()
+    {
+        var enemiesAlive = GameObject.FindGameObjectsWithTag("Enemy");
+
+        if (enemiesAlive.Length == 0 && _availableSpawnAreas.Count == 0)
         {
-            var enemiesToSpawn = Mathf.RoundToInt(spawnPercentages[i] / 100f * totalEnemiesInWave);
-            enemiesToSpawnPerPoint.Add(enemiesToSpawn);
+            StartNextWave();
         }
 
-        return enemiesToSpawnPerPoint;
-    }
-
-    // Spawns enemies with a delay in between spawns
-    private IEnumerator SpawnWaveWithDelay()
-    {
-        while (availableSpawnPoints.Count > 0)
+        if (Input.GetKeyDown(KeyCode.F))
         {
-            var randomSpawnPoint = Random.Range(0, availableSpawnPoints.Count);
-
-            var spawnedEnemy = Instantiate(enemyPrefab, availableSpawnPoints[randomSpawnPoint].position, availableSpawnPoints[randomSpawnPoint].rotation);
-            numberEnemiesPerPoint[randomSpawnPoint]--;
-
-            if (numberEnemiesPerPoint[randomSpawnPoint] <= 0)
+            foreach (var enemy in enemiesAlive)
             {
-                availableSpawnPoints.RemoveAt(randomSpawnPoint);
-                numberEnemiesPerPoint.RemoveAt(randomSpawnPoint);
+                Destroy(enemy);
             }
-
-            yield return new WaitForSeconds(spawnDelay);
         }
+    }
+
+    // Calculates and returns a list of int that represents the number of enemies to spawn per spawn areas according to percentages given
+    private List<int> CalculateEnemiesPerSpawnArea()
+    {
+        var enemiesToSpawnPerArea = new List<int>();
+
+        for (var i = 0; i < spawnAreas.Count; i++)
+        {
+            var enemiesToSpawn = Mathf.RoundToInt(spawnAreas[i].spawnPercentage / 100f * _totalEnemiesInWave);
+            enemiesToSpawnPerArea.Add(enemiesToSpawn);
+        }
+
+        Debug.Log(_currentWave);
+
+        for (var i = 0; i < spawnAreas.Count; i++)
+        {
+            Debug.Log("Spawning " + enemiesToSpawnPerArea[i] + " in " + spawnAreas[i].pathName);
+        }
+
+        return enemiesToSpawnPerArea;
+    }
+
+    // Spawns enemies with a random delay between spawns
+    private IEnumerator SpawnWaveWithRandomDelay()
+    {
+        while (_availableSpawnAreas.Count > 0)
+        {
+            var randomSpawnAreaIndex = Random.Range(0, _availableSpawnAreas.Count);
+            var spawnArea = _availableSpawnAreas[randomSpawnAreaIndex];
+
+            var spawnAreaCollider = spawnArea.areaGameObject.GetComponent<Collider>();
+
+            if (spawnAreaCollider != null)
+            {
+                var spawnBounds = spawnAreaCollider.bounds;
+
+                var randomX = Random.Range(spawnBounds.min.x, spawnBounds.max.x);
+                var randomZ = Random.Range(spawnBounds.min.z, spawnBounds.max.z);
+
+                var randomSpawnPoint = new Vector3(randomX, spawnBounds.center.y, randomZ);
+
+                var enemy = Instantiate(_enemyPrefab, randomSpawnPoint, Quaternion.identity);
+
+                //Randomize speed for each enemy
+                var randomSpeedMultiplier = Random.Range(0.8f, 1.5f);
+                enemy.GetComponent<EnemyMovement>().speed *= randomSpeedMultiplier;
+
+                _numberEnemiesPerArea[randomSpawnAreaIndex]--;
+
+                if (_numberEnemiesPerArea[randomSpawnAreaIndex] <= 0)
+                {
+                    _availableSpawnAreas.RemoveAt(randomSpawnAreaIndex);
+                    _numberEnemiesPerArea.RemoveAt(randomSpawnAreaIndex);
+                }
+
+                var randomDelay = Random.Range(minDelay, maxDelay);
+                yield return new WaitForSeconds(randomDelay);
+            }
+        }
+    }
+
+    private void StartNextWave()
+    {
+        _currentWave++;
+        CalculateAmountOfEnemies();
+        _availableSpawnAreas = new List<SpawnArea>(spawnAreas);
+        _numberEnemiesPerArea = CalculateEnemiesPerSpawnArea();
+        StartCoroutine(SpawnWaveWithRandomDelay());
     }
 }
