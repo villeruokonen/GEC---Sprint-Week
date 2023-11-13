aa4f69f466ec2d1d8c708e68c24710bff129004c
Oğuzhan YILDIZ <oguzhan00yildiz@gmail.com>
2023-11-13 23:23:26 +0200

    Restart game changed

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
     [SerializeField] private GameObject _failScreen;
     [SerializeField] private GameObject _totem;
     [SerializeField] private float _totemFailY = -10.0f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -26,5 +28,14 @@ public class GameController : MonoBehaviour
     void ShowFailScreen()
     {
         _failScreen?.SetActive(true);
+        StartCoroutine(RestartGame());
     }
+
+    IEnumerator RestartGame()
+    {
+        yield return new WaitForSeconds(2.0f);
+        SceneManager.LoadScene(0);
+    }
+
+   
 }
